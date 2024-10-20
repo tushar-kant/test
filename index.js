dd39360c1a5577d9fb25cc86036caede8cbc9a2f
akashkumarnayak2004 <ece.190410029@silicon.ac.in>
2024-10-20 08:32:16 +0530

    test1

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ connectDB();
 
 // Import and initialize the bot
 require('./bot');
+
+// Create an Express app if you need the server to listen on a port
+const express = require('express');
+const app = express();
+
+// Set up a simple route to check if the server is running
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Start the server and listen on a port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
